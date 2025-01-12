abf82e559b111375a12e3a005a6235875c6aeeba
tomas <tomasboob@gmail.com>
2025-01-12 20:13:23 +0100

    Initial Commit

---
+++ RecreateCashAppNumpad/RecreateCashAppNumpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecreateCashAppNumpadApp.swift
+//  RecreateCashAppNumpad
+//
+//  Created by Tomás Boo Becerra on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct RecreateCashAppNumpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
